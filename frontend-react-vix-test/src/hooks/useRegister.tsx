99dc0bff474ccd2339d02cc04f87e7c1502a617a
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-01-21 15:21:57 -0300

    feature: adiciona registro de usuário via front-end

--- frontend-react-vix-test/src/hooks/useRegister.tsx
+++ frontend-react-vix-test/src/hooks/useRegister.tsx
@@ -1,12 +1,10 @@
 import { useTranslation } from "react-i18next";
 import { toast } from "react-toastify";
 import { api } from "../services/api";
-import { useZBrandInfo } from "../stores/useZBrandStore";
 import { useNavigate } from "react-router-dom";
 
 export const useRegister = () => {
   const { t } = useTranslation();
-  const { idBrand } = useZBrandInfo();
   const navigate = useNavigate();
 
   const validateEmail = (email: string) => {
@@ -47,12 +45,11 @@ export const useRegister = () => {
     }
 
     const response = await api.post({
-      url: "/user",
+      url: "/auth/register",
       data: {
         username,
         password,
         email,
-        idBrandMaster: idBrand,
       },
     });
 
